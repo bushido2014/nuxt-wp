24bb4598e0878e275ba582ff464a8420904cab4d
bushido2014 <joomjoom2011@gmail.com>
2022-12-08 16:28:58 +0200

    add section Skills

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
     <Hero />
+    <section class="skills" id="skills">
+      <div class="section_title">
+        <h1>SKILLS</h1>
+      </div>
+      <div class="container">
+        <Skills />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import Hero from '~/components/Hero';
+import Skills from '~/components/Skills';
 export default {
-  components: { Hero },
+  components: { Hero, Skills },
+  head() {
+    return {
+      title: 'Viorel Soltan - Front End Developer',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Website with Nuxt.js and WordPress REST API',
+        },
+      ],
+      link: [
+        {
+          rel: 'canonical',
+          href: 'https://dev-websoltan.com' + this.$route.path,
+        },
+      ],
+    };
+  },
 };
 </script>
