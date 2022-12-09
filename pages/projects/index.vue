13147a609d48fa387204800926b2a7b1e9f2b56f
bushido2014 <joomjoom2011@gmail.com>
2022-12-10 00:45:03 +0200

    3 files modified

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-    <h2>All Projects</h2>
-
-    <ProjectCard />
-  </div>
+  <section class="all-projects">
+    <div class="container">
+      <div v-if="projects" class="project-wrapper grid">
+        <ProjectCard
+          v-for="(project, i) in projects"
+          :key="i"
+          class=""
+          :project="project"
+        />
+      </div>
+    </div>
+  </section>
 </template>
+
 <script>
 import ProjectCard from '~/components/ProjectCard';
 export default {
-  components: {
-    ProjectCard,
+  components: { ProjectCard },
+  async asyncData({ $axios }) {
+    const projects = await $axios.$get('/wp-json/wp/v2/project');
+    return { projects };
+  },
+  head() {
+    return {
+      title: 'All Projects - Nuxt JS',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Website  Development, Vue, Nuxt.js and WordPress',
+        },
+      ],
+      link: [
+        {
+          rel: 'canonical',
+          href: 'https://dev-websoltan.com' + this.$route.path,
+        },
+      ],
+    };
   },
 };
 </script>
