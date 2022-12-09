bc6ae4f2556f31eef4ef8faf0d6796d614601590
bushido2014 <joomjoom2011@gmail.com>
2022-12-09 10:40:27 +0200

    1 file modified, 3 added

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,39 @@
         <Skills />
       </div>
     </section>
+    <section class="home-projects">
+      <div class="section_title">
+        <h1>Projects</h1>
+      </div>
+      <div class="container">
+        <div class="project-list grid">
+          <nuxt-link
+            v-for="(project, i) in projects"
+            :key="i"
+            :to="`/project/${project.slug}/`"
+            class="project-list__card"
+          >
+            <ProjectList :project="project" :no-text="true" />
+          </nuxt-link>
+        </div>
+        <div class="project-list-link">
+          <nuxt-link to="/project" class="button">View All Projects </nuxt-link>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import Hero from '~/components/Hero';
 import Skills from '~/components/Skills';
+import ProjectList from '~/components/ProjectList';
 export default {
-  components: { Hero, Skills },
+  components: { Hero, Skills, ProjectList },
+  async getProjects({ $axios }) {
+    const project = await $axios.$get('/wp-json/wp/v2/project?per_page=3');
+    return { project };
+  },
   head() {
     return {
       title: 'Viorel Soltan - Front End Developer',
