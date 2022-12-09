7b5ec1658d01e2e2efcfd81a6da4f9591769ed5a
bushido2014 <joomjoom2011@gmail.com>
2022-12-09 14:23:54 +0200

    3 files added

---
+++ pages/projects/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-if="projects" class="container mx-auto my-20 px-6">
+      <project-card
+        v-for="(project, i) in projects"
+        :key="i"
+        :project="project"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import ProjectCard from '~/components/ProjectCard';
+export default {
+  components: { ProjectCard },
+  async asyncData({ $axios }) {
+    const projects = await $axios.$get('/wp-json/wp/v2/project');
+    return { projects };
+  },
+  head() {
+    return {
+      title: 'All Projects - Nuxt JS',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Website  Development, Vue, Nuxt.js and WordPress Related Articles Related Articles, Tips , Web Hosting deals coupons and more.',
+        },
+      ],
+    };
+  },
+};
+</script>
